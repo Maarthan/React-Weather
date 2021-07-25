f3e32b5f68f508ebbf6b83a4792dd4c278ff3ea8
Manoj M <manojmbm7@gmail.com>
2021-07-25 22:26:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import WeatherInfo from "./components/WeatherInfo";
 
 function App() {
+  // used to connect to the input field where user enters the city name
+  const [cityName, setCityName] = useState("");
+  // Used to pass the cityName after the button is clicked
+  // this extra state is to prevent the api hit of weather when user is typing
+  const [city, setCity] = useState("");
+  // state to render the weather prop based on a boolean value
+  const [showWeather, setShowWeather] = useState(false);
+
+  // on click of the get weather info button
+  const onSubmit = () => {
+    // checking to see if city name is empty
+    if (cityName !== "") {
+      console.log(cityName);
+      // setting the showWeather to true so that the component is rendered
+      setShowWeather(true);
+      // setting the city variable so it will be passed to WeatherInfo component
+      setCity(cityName.trim());
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Div to hold input and button */}
+      <div>
+        {/* Input field to get the user input for a city name */}
+        {/* value is set to cityName state */}
+        {/* when something changes i.e., kep press the event will be triggered and the state will be set / changed */}
+        <input
+          type="text"
+          placeholder="Enter a city name"
+          value={cityName}
+          onChange={(e) => setCityName(e.target.value)}
+        />
+        {/* button to submit the value to the component as mentioned in the onSubmit funtion */}
+        <button type="button" onClick={onSubmit}>
+          Get Weather Info
+        </button>
+      </div>
+      {/* Conditionally rendering the WeatherInfo component based on the state's value */}
+      {/* Sending down the cityName as prop to WeatherInfo component */}
+      {showWeather && <WeatherInfo cityName={city} />}
     </div>
   );
 }
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+
+const WeatherInfo = ({ cityName }) => {
+  // Base url to connect to the open weather map website and its api
+  // cityName is a variable and is passed as a prop from parent component i.e., App.js
+  // Note: paste the API key from open weather map website after login
+  const API_KEY = "6d042fbd6ad58f89f60a767916546774";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  // Weather info from the api
+  const [weather, setWeather] = useState(null);
+  // error after hitting the api either city not found or API not there
+  const [error, setError] = useState(null);
+  // Loding state while the api is hit
+  const [pending, setPending] = useState(true);
+
+  // used to run when the cityName changes
+  useEffect(() => {
+    // fetch API from javascript which returns a promise
+    fetch(url)
+      // returns a respone with meta data and actual data
+      .then((res) => {
+        // if the Http status of the response is not in the range of 201 to 299 then something went wrong
+        if (!res.ok) {
+          throw Error("City not found");
+        }
+        // returning the json data from response
+        return res.json();
+      })
+      // reciving data
+      .then((data) => {
+        console.log(data);
+        // stopping the loading screen
+        setPending(false);
+        // setting the error to null as data is recieved
+        setError(null);
+        // setting the data recived from api
+        setWeather(data);
+      })
+      .catch((error) => {
+        // setting the data to null as error has occured
+        setWeather(null);
+        // stopping the loading screen
+        setPending(false);
+        // setting the error recieved
+        setError(error);
+      });
+  }, [cityName]);
+
+  // the temparatures recieved from api are Kelvin, the below function converts the kelvin temparature to celsius
+  const kelvinToCelsius = (temp) => {
+    return Math.floor(temp - 273.15);
+  };
+
+  return (
+    <div>
+      {/* conditionally rendering the loading screen */}
+      {pending && <p>Loading...</p>}
+      {/* conditionally rendering the error */}
+      {error && <p>{error.message}</p>}
+      {/* conditionally rendering the weather info */}
+      {weather && (
+        <div>
+          {/* check the log of weather for the whole data and change the below code accordingly */}
+          <h1>
+            Showing weather info for {weather.name}, {weather.sys.country}
+          </h1>
+          <h2>Weather summary: {weather.weather[0].description}</h2>
+          <p>Temparature: {kelvinToCelsius(weather.main.temp)}&#8451;</p>
+          <p>
+            Maximum Temparature: {kelvinToCelsius(weather.main.temp_max)}&#8451;
+          </p>
+          <p>
+            Minimum Temparature: {kelvinToCelsius(weather.main.temp_min)}&#8451;
+          </p>
+          <p>Feels like: {kelvinToCelsius(weather.main.feels_like)}&#8451;</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherInfo;
